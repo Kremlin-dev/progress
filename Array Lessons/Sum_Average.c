17f0447601e271d55d26b29c57ac88177cf1b47d
AI-Amponsah <Yawamp27@gmail.com>
2022-09-30 01:09:41 +0000

    Sum and Average of an array

--- Array Lessons/Sum_Average.c
+++ Array Lessons/Sum_Average.c
@@ -11,20 +11,19 @@
     printf("Enter five random values: \t");
     for(i=0;i<5;i++){
         scanf("%d",&num[i]);
-    }
-    for(i=0; i<5;i++){
-        sum = sum + num[i];
-    }
+         sum = sum + num[i];
+          Deviation = num[i]-average;
+        }
+    
     average = sum/5;
         
      printf("Sum of the values is: %d\n",sum);
      printf("The average of the values is: %.1f \n\n\n",average);
 
-        for(i=0;i<5;i++){
-             Deviation = num[i]-average;
+    for(i=0;i<5;i++){
         printf("Deviation of the value %d from mean is: %.1f\n",num[i],Deviation);
-        
-    }
-
+        }
+    
+    
     return 0;
  }
