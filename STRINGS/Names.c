ebe67c9f7093a2a7936bf5053e4a55c23c0cdd74
AI-Amponsah <Yawamp27@gmail.com>
2022-10-03 01:41:24 +0000

    Progress_Project

--- STRINGS/Names.c
+++ STRINGS/Names.c
@@ -11,16 +11,25 @@ int main(){
     char name[300];
     int i,hour;
 // Using a for loop to write and read from a file
+    fprintf(out, "NAME");
+    fprintf(out, "  \t\t");
+    fprintf(out, "HOURS");
+    fprintf(out, "  \t\t");
+    fprintf(out, "RATE");
+    //fprintf(out, "  \t");
+    //fprintf(out, "RATE\t\t\t");
+/**
     for(i=0; i<7;i++){
     fscanf(in, "%s %s %d",firstName,lastName,&hour);
     strcpy(name, firstName);
     strcat(name, " ");
     strcat(name, lastName);
-    fprintf(out, "%s\t %20d\n",name,hour);
+    fprintf(out, "%s\t %d\n",name,hour);
     //fprintf(out, "%5d",hour);
     } 
     fclose(in);
     fclose(out);
+    **/
     
     return 0;
 }
