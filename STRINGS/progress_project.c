b8c5a589a403ec3531c2000e3e76b72945b8e3bc
AI-Amponsah <Yawamp27@gmail.com>
2022-10-03 06:32:49 +0000

    Progress_project

--- STRINGS/progress_project.c
+++ STRINGS/progress_project.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-// Reading and writing into a file
+// This code Reads and writes data into a file
 int main(){
 // Declaring file pointers and modes
     FILE *in =fopen("name.txt","r");
     FILE *out =fopen("proj.txt","w");
+// Declaring characters
     char firstName[100];
     char lastName[100];
     char name[300];
@@ -16,11 +17,13 @@ int main(){
     for(i=0; i<4;i++){
     fscanf(in, "%s",firstName);
     fscanf(in, " %s %d %d",lastName,&hour,&rate);
+//string functions to concatenate and copy string inputs
     strcpy(name, firstName);
     strcat(name, "  ");
     strcat(name, lastName);
     fprintf(out, "%s\t %10d %10d",name,hour,rate);
     fprintf(out,"\n");
+    }
     fclose(in);
     fclose(out);
 
