cfb6ee51f3d3ff439293a301faa3a428a92c228d
AI-Amponsah <Yawamp27@gmail.com>
2022-10-06 02:52:58 +0000

    Character_set

---
+++ Character/char.c
@@ -0,0 +1,9 @@
+#include  <stdio.h>
+#include <stdlib.h>
+//This programs is to illustrate basic operations of characters in C
+
+int main(){
+    char alph = 'a';
+    printf("%c",alph-32);
+    return 0;
+}
