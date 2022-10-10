e9859bad3cfe750546a9ba243fd4a0d387edbe51
AI-Amponsah <Yawamp27@gmail.com>
2022-10-10 03:44:19 +0000

    Pass by reference

---
+++ POINTERS/pass-by_reference.c
@@ -0,0 +1,18 @@
+//This program receives two values from a user and swaps them by passing by reference
+#include <stdio.h>
+//Void function prototype with pointer arguments
+void change(int *x,int *y);
+
+int main (){
+    int m = 10,n =20;
+    change(&m,&n);
+    printf("%d %d",m,n);
+    return 0;
+}
+//function definition
+void change(int *x,int *y)
+{
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
