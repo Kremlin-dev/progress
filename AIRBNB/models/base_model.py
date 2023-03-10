02cd85cd7c7a0a822166f865a536f8dd2499352c
AI-Amponsah <yawmamp27@gmail.com>
2023-03-10 11:22:44 +0000

    merge

--- AIRBNB/models/base_model.py
+++ AIRBNB/models/base_model.py
@@ -14,8 +14,9 @@ class BaseModel():
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
+            self.created_at = datetime.now()
     def __str__(self):
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}" 
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
     def save(self):
         self.updated_at = datetime.now()
     def to_dict(self):
@@ -24,4 +25,3 @@ class BaseModel():
         dict_copy["created_at"]= self.created_at.isoformat()
         dict_copy["updated_at"] = self.updated_at.isoformat()
         return dict_copy
-
