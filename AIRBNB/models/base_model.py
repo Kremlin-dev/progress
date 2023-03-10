5898261b62263ae8454c41b064de00326a1f4922
AI-Amponsah <yawmamp27@gmail.com>
2023-03-10 11:36:06 +0000

    m

--- AIRBNB/models/base_model.py
+++ AIRBNB/models/base_model.py
@@ -16,11 +16,13 @@ class BaseModel():
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+             storgae.new(self)
     def __str__(self):
         return f"[{type(self).__name__}] ({self.id}) {self.__dict__}" 
     def save(self):
         self.updated_at = datetime.now()
-	storgae.save(self)
+        storgae.save(self)
     def to_dict(self):
         dict_copy = self.__dict__.copy()
         dict_copy["__class__"] = type(self).__name__
---
+++ PRACTICE/Hello.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+// stdio : stands for standard input/output
+// printf();
+
+// scanf();
+
+//GNU  : gcc and g++
+
+int main(void)
+{
+    printf("Hello World!");
+    return (0);
+}
+
