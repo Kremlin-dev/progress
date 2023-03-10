d8fc56bb31a217e40a66e84a5f5d4e6689977875
AI-Amponsah <yawamp27@gmail.com>
2023-03-10 11:47:36 +0000

    m

--- AIRBNB/models/base_model.py
+++ AIRBNB/models/base_model.py
@@ -17,7 +17,7 @@ class BaseModel():
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-             storgae.new(self)
+            storgae.new(self)
     def __str__(self):
         return f"[{type(self).__name__}] ({self.id}) {self.__dict__}" 
     def save(self):
