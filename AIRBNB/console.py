6c12298ca619dabcac4de47beb759f8016e00153
AI-Amponsah <yawamp27@gmail.com>
2023-03-10 16:21:15 +0000

    console

--- AIRBNB/console.py
+++ AIRBNB/console.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 import cmd
+from models.base_model import BaseModel
 class HBNBCommand(cmd.Cmd):
 	prompt = "(hbnb) "
+	__classes = {"BaseModel"}
 	def do_quit(self, line):
 		"""Quit command to exit console 
 		"""
@@ -14,6 +16,18 @@ class HBNBCommand(cmd.Cmd):
 		return True
 	def emptyline(self):
 		return
+	def do_create(self, line):
+		args = line.split()
+		if len(args) == 0:
+			print("** class name missing **")
+		elif args[0] not in (self.__classes):
+			print("** class doesn't exist")
+		else:
+		 new_instance = eval(f"{args[0]}")()
+		 print(new_instance.id)
+		 
+		     
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
