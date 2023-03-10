6c48d3ac5aec179cf301dd7c100a2d1e8cebb672
AI-Amponsah <yawamp27@gmail.com>
2023-03-10 11:21:05 +0000

    storage

--- AIRBNB/models/base_model.py
+++ AIRBNB/models/base_model.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 import uuid
 from datetime import datetime
+from models.engine.__init__ import storage
+
 class BaseModel():
     def __init__(self, *args, **kwargs):
         if len(kwargs) != 0:
@@ -15,10 +17,12 @@ class BaseModel():
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+	    storage.new(self)
     def __str__(self):
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
     def save(self):
         self.updated_at = datetime.now()
+	storgae.save(self)
     def to_dict(self):
         dict_copy = self.__dict__.copy()
         dict_copy["__class__"] = self.__class__.__name__
