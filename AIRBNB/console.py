e7aadbe39f0f46a752392c742759be8217023d8d
AI-Amponsah <yawamp27@gmail.com>
2023-03-10 14:59:14 +0000

    EOF

--- AIRBNB/console.py
+++ AIRBNB/console.py
@@ -3,10 +3,16 @@ import cmd
 class HBNBCommand(cmd.Cmd):
 	prompt = "(hbnb) "
 	def do_quit(self, line):
+		"""Quit command to exit console 
+		"""
+
 		return True
 	def do_EOF(self, line):
+		""""Signal to exit program
+		"""
+
 		return True
-	def emptyline():
+	def emptyline(self):
 		return
 
 if __name__ == '__main__':
