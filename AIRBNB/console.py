138001d13cd6701edb23561ba0c00752ad48dad3
AI-Amponsah <yawamp27@gmail.com>
2023-03-11 12:25:04 +0000

    base

--- AIRBNB/console.py
+++ AIRBNB/console.py
@@ -2,9 +2,10 @@
 import cmd
 from models.base_model import BaseModel
 from models import storage
+from models.user import User
 class HBNBCommand(cmd.Cmd):
 	prompt = "(hbnb) "
-	__classes = {"BaseModel"}
+	__classes = ["BaseModel", "User"]
 	def do_quit(self, arg):
 		"""Quit command to exit console 
 		"""
@@ -54,6 +55,30 @@ class HBNBCommand(cmd.Cmd):
 		else:
 			del storage.all()[f"{args[0]}.{args[1]}"]
 		storage.save()
+	def do_all(self,arg):
+		args = arg.split()
+		if (len(args) == 0):
+			print([str(value) for value in storage.all().values()])
+		elif (args[0] not in self.__classes):
+			print("** class doesn't exist **")
+		else: 
+		 	print([str(v) for k, v in storage.all().items() if k.startswith(args[0])])
+	def do_update(self, arg):
+		args = arg.split()
+
+		if(len(args)== 0):
+			print("** class name missing **")
+		elif (args[0] not in self.__classes):
+			print("** class doesn't exist **")
+		elif (len(args) == 1):
+			print("** instance id missing **")
+		elif f"{args[0]}.{args[1]}" not in storage.all():
+			print("** no instance found **")
+		elif (len(args) == 2):
+			print("** attribute name missing **")
+		elif (len(args) == 3):
+			print("** value missing **")
+
 
 	 
 if __name__ == '__main__':
