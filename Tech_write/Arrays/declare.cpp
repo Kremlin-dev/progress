3cde1e73de557cbd128fa8db21e9e99c537ac723
AI-Amponsah <yawamp27@gmail.com>
2023-05-19 00:54:08 +0000

    arrays

---
+++ Tech_write/Arrays/declare.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int myArray[5];
+
+    /**
+    Declaring an array of type int
+    Array size: 2
+    Array name: myArray
+    */
+    return (0);
+}
---
+++ Tech_write/Arrays/initialize.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int myArray[] = {2, 4, 5, 6};
+   //size: 5
+
+
+    return (0);
+}
