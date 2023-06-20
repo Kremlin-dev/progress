b5420c42c74d6f6409aecf4af4bc36f526ab141f
AI-Amponsah <yawamp27@gmail.com>
2023-06-20 13:09:19 +0000

    recent

---
+++ FLASK_TUTORIALS/project_1.0/app.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:krem@localhost/flasktry'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+class Person(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    FirstName = db.Column(db.String(40), nulllable=False)
+    LastName = db.Column(db.String(40), nullable= False)
+
+    def __init__(self, FirstName, LastName):
+        self.FirstName = FirstName
+        self.LastName =LastName
+
+@app.route('/')
+def homepage():
+    return "Hello World"
+@app.route('/login', methods=['post'])
+def login():
+    FirstName =request.form["FirstName"]
+    LastName =request.form["LasttName"]
+    entry = Person(FirstName, LastName)
+    db.session.add(entry)
+    db.commit()
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    db.create_all()
+    app.run()
