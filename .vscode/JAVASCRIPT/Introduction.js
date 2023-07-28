d78ae745d43cc9ba3a58fd726d0811005998b1b7
AI-Amponsah <yawamp27@gmail.com>
2023-07-28 02:57:58 +0000

    push

--- .vscode/JAVASCRIPT/Introduction.js
+++ .vscode/JAVASCRIPT/Introduction.js
@@ -1,10 +1,18 @@
 //Varibale declariation
 
 var x = 7;
-console.log(x);
+// console.log(x);
 
 let y = 45;
 console.log(y);
 
 const z = 65;
 console.log(z);
+
+ function product (x, y)
+{
+    return x *y;
+}
+
+let w = product(2, 5);
+console.log(w);
