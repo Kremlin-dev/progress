22b205ea29fdfba1209f1faebb9c83d4068f0d88
AI-Amponsah <yawamp27@gmail.com>
2023-09-11 19:55:37 +0000

    env

--- FLASK_TUTORIALS/1.0/app.py
+++ FLASK_TUTORIALS/1.0/app.py
@@ -2,15 +2,16 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URL']=''
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://zwuvjhek:I1AhzNgkSJr5Dp-sfvWe4NL3kKN9o1XT@dumbo.db.elephantsql.com/zwuvjhek'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
 class user(db.Model):
-    id = db.Column(db.Integer, Primary_key=True)
-    firstName = db.Column(db.string, nullable=False)
-    lastName = db.Column(db.string, nullable=False)
-    Email = db.Column(db.string, nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    firstName = db.Column(db.String, nullable=False)
+    lastName = db.Column(db.String, nullable=False)
+    Email = db.Column(db.String, nullable=False)
 
 
 if __name__ == "__main__":
