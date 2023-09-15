4b5e75c7dd4ed3531aa6d0c608ca6ba4a5268a47
AI-Amponsah <yawamp27@gmail.com>
2023-09-15 09:17:00 +0000

    some test files

---
+++ TESTS/mail.py
@@ -0,0 +1,35 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.application import MIMEApplication
+
+smtp_server = 'smtp.gmail.com'
+smtp_port = 587
+smtp_username = 'yawamp27@gmail.com'
+smtp_password = 'qmiatkbgtzghitya' 
+
+subject = 'Internship form'
+message = 'I am trying it out'
+
+msg = MIMEMultipart()
+msg['From'] = smtp_username
+msg['To'] = 'johnmyles523@gmail.com'
+msg['Subject'] = subject
+msg.attach(MIMEText(message, 'plain'))
+path = file_path = './m.pdf'  
+
+with open(path, 'rb') as attachment:
+    part = MIMEApplication(attachment.read(), Name='m.pdf')
+
+part['Content-Disposition'] = f'attachment; filename="m.pdf"'
+msg.attach(part)
+
+try:
+    server = smtplib.SMTP(smtp_server, smtp_port)
+    server.starttls()
+    server.login(smtp_username, smtp_password)
+    server.sendmail(smtp_username, 'johnmyles523@gmail.com', msg.as_string())
+    server.quit()
+    print("Email with attachment sent successfully!")
+except Exception as e:
+    print(f"Error sending email: {str(e)}")
---
+++ TESTS/mail2.py
@@ -0,0 +1,27 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+smtp_server = 'smtp.gmail.com'
+smtp_port = 587
+smtp_username = 'yawamp27@gmail.com'
+smtp_password = 'qmiatkbgtzghitya' 
+
+subject = 'Greetings'
+message = 'Hello Myles'
+
+msg = MIMEMultipart()
+msg['From'] = smtp_username
+msg['To'] = 'johnmyles523@gmail.com'
+msg['Subject'] = subject
+msg.attach(MIMEText(message, 'plain'))
+
+try:
+    server = smtplib.SMTP(smtp_server, smtp_port)
+    server.starttls()
+    server.login(smtp_username, smtp_password)
+    server.sendmail(smtp_username, 'johnmyles523@gmail.com', msg.as_string())
+    server.quit()
+    print("Email sent successfully!")
+except Exception as e:
+    print(f"Error sending email: {str(e)}")
