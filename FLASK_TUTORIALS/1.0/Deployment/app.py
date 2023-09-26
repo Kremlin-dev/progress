e0bbecc0d3b5996fcda13f03c825c0fd89c8e2de
AI-Amponsah <yawamp27@gmail.com>
2023-09-26 13:25:12 +0000

    work

--- FLASK_TUTORIALS/1.0/Deployment/app.py
+++ FLASK_TUTORIALS/1.0/Deployment/app.py
@@ -1,6 +1,13 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template
 
-@app.route('/')
-def home():
-    return "Hello World"
+app = Flask(__name__, template_folder="templates")
+app.secret_key = "krem"
+app.static_folder = "static"
+@app.route('/', methods=['GET'])
+def signup():
+    return render_template("signup.html")
+
+
+
+if __name__ == '__main__':
+    app.run()
