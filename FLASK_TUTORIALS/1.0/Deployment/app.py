037d9f51ab81cc56c3f35e01e656da010c58786a
AI-Amponsah <yawamp27@gmail.com>
2023-09-29 10:54:38 +0000

    react test

--- FLASK_TUTORIALS/1.0/Deployment/app.py
+++ FLASK_TUTORIALS/1.0/Deployment/app.py
@@ -1,13 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify, request
+from flask_cors import CORS
 
-app = Flask(__name__, template_folder="templates")
-app.secret_key = "krem"
-app.static_folder = "static"
-@app.route('/', methods=['GET'])
+app = Flask(__name__)
+CORS(app, resources={r"/api/*": {"origins": "*"}})
+
+@app.route('/api/signup', methods=['POST', 'OPTIONS'])
 def signup():
-    return render_template("signup.html")
+    if request.method == 'OPTIONS':
+        return jsonify({'message': 'Preflight request received'}), 200
 
+    data = request.get_json()
+    print('Received signup request:', data)
 
+    username = data.get('username')
+    password = data.get('password')
+    print(f"Username: {username}, Password: {password}")
+
+    return jsonify({'message': 'User signed up successfully'})
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
---
+++ FLASK_TUTORIALS/1.0/Deployment/my-react-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import SignupComponent from './signup';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to My React App</h1>
+      <SignupComponent />
+      {/* Add more components or content as needed */}
+    </div>
+  );
+}
+
+export default App;
