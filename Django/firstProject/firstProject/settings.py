74dd17e086b33de1e00c8d4ec3dfdd7586bcf1ac
Kremlin-dev <yawamp27@gmail.com>
2024-06-20 17:43:00 +0000

    Introduction to Django

--- Django/firstProject/firstProject/settings.py
+++ Django/firstProject/firstProject/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'firstProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'kremschema',
+        'USER': 'root',
+        'PASSWORD':'2704',
+        'HOST': 'localhost',
+        'PORT':'3306'
     }
 }
 
