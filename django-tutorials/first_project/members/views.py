4b1d84db8b0aff1861b6aa9f3060cbbcc11fdba6
Kremlin-dev <yawamp27@gmail.com>
2024-09-05 19:05:38 +0000

    django app

--- django-tutorials/first_project/members/views.py
+++ django-tutorials/first_project/members/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+def home(request):
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render())
+
+def login(request):
+    return HttpResponse("please login")
 
 # views are python functions that take Http request and return http responses
 
