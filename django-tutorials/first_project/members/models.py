cca39a8f3221a68bf6fdfc9b8f30c801697baf3b
Kremlin-dev <yawamp27@gmail.com>
2024-09-06 09:46:07 +0000

    models update

--- django-tutorials/first_project/members/models.py
+++ django-tutorials/first_project/members/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+class login(models.Model):
+    username = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+
 # Create your models here.
