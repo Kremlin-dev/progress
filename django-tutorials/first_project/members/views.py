5376c6c14e642b9af5da9608de568df427c8e019
Kremlin-dev <yawamp27@gmail.com>
2024-09-06 10:51:41 +0000

    working with django tags

--- django-tutorials/first_project/members/views.py
+++ django-tutorials/first_project/members/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import login
 
 def home(request):
-    template = loader.get_template('index.html')
-    return HttpResponse(template.render())
 
-def login(request):
-    return HttpResponse("please login")
+    users = login.objects.all().values()
+    print(users)
+    template = loader.get_template('index.html')
+    context ={
+        'users': users,
+    }
+    return HttpResponse(template.render(context, request))
+
+# def login(request):
+#     return HttpResponse("please login")
 
 # views are python functions that take Http request and return http responses
 
