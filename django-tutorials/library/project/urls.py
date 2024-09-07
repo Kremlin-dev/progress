f9c1a1c498a66dbb0241911162311d3f67c2224f
Kremlin-dev <yawamp27@gmail.com>
2024-09-07 02:30:06 +0000

    changes made

--- django-tutorials/library/project/urls.py
+++ django-tutorials/library/project/urls.py
@@ -3,8 +3,9 @@ from . import views
 
 
 urlpatterns = [
+    path('', views.home, name='homepage'),
     path('bookslist/', views.book_list, name='booklist'),
-    path('bookdetails/', views.book_detail, name='bookdetail'),
+    path('bookdetails/<int:id>', views.book_detail, name='bookdetail'),
     path('addbook/', views.add_book, name='addbook'),
     path('updatebook/', views.update_book, name='updatebook'),
     path('deletebook/', views.delete_book, name='deletebook'),
