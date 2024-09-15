471ede22978c6fadbbee9582663ac04d34a4620e
Kremlin-dev <yawamp27@gmail.com>
2024-09-15 00:59:16 +0000

    created models and run migrations

--- django-tutorials/formproject/form/forms.py
+++ django-tutorials/formproject/form/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 
 class userRegistration(forms.Form):
-    firstName = froms.CharField(label="firstName", max_length=25)
+    firstName = forms.CharField(label="firstName", max_length=25)
     lastName = forms.CharField(label="lastName", max_length = 25)
-    email = forms.EmailField(label="email", max_length=255, unique=True, help_text="Please use the correct email fornat")
-    telephone = forms.CharField(label="telephone", max_length=10, unique=True, help_text="Please use the correct telephone fornat")
+    email = forms.EmailField(label="email", max_length=255, help_text="Please use the correct email fornat")
+    telephone = forms.CharField(label="telephone", max_length=10, help_text="Please use the correct telephone fornat")
