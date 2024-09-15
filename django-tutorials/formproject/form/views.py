0a60ddcba18414a0b3f17cb3875bd9e4ab8b2791
Kremlin-dev <yawamp27@gmail.com>
2024-09-15 00:54:18 +0000

    working on views.py

--- django-tutorials/formproject/form/views.py
+++ django-tutorials/formproject/form/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import render, loader
+from django.http import HttpResponse
+from .models import register
+from .forms import userRegistration
+
+
+
+def home(request):
+
+    template = loader.get_template("index.html")
+
+    return HttpResponse(template.render())
+
+
+def signup(request):
+    if request.method == "POST":
+        firstName = request.POST.get("firstName")
+        lastName = request.POST.get("lastName")
+        email = request.POST.get("email")
+        telephone = request.POST.get("telephone")
+
+        newdata = register(
+
+            firstName=firstName,
+            lastName = lastName,
+            email = email,
+            telephone = telephone
+        )
+        newdata.save()
+
+        template = loader.get_template("form.html")
+
+        message = "Data Receved and saved sucessfully"
+        return HttpResponse(template.render(request, message=message))
+
+    form = userRegistration()
+
+    context = {
+        "form" : form
+    }
+
+    template = laoder.get_template("form.html")
+
+    return HttpResponse(template.render(request, context))
+
+
+
 
 # Create your views here.
