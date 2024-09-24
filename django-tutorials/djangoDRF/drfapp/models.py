6745cd9767cc36472d60dab2675827792475aada
Kremlin-dev <yawamp27@gmail.com>
2024-09-24 10:01:40 +0000

    creating note api

--- django-tutorials/djangoDRF/drfapp/models.py
+++ django-tutorials/djangoDRF/drfapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+class Note(models.Model):
+    title = models.CharField(max_length = 255)
+    content = models.TextField()
+    created_at = models.DateField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    owner = models.CharField(max_length=True)
+
+    def __str__(self):
+        return self.title
+
 # Create your models here.
--- django-tutorials/djangoDRF/drfapp/views.py
+++ django-tutorials/djangoDRF/drfapp/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+
+@api_view['POST']
+def create_note(request):
+    pass
+
+@api_view['GET']
+def get_note(request):
+    pass
+
+
+@api_view['PATCH']
+def update_note(request):
+    pass
+
+
+@api_view['DELETE']
+def delete_note(request):
+    pass
+
+
 # Create your views here.
