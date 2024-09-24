17ebe71c4f40d887946ed79f394b26add19ccae5
Kremlin-dev <yawamp27@gmail.com>
2024-09-24 10:23:07 +0000

    created seralizer and tokenpair

--- django-tutorials/djangoDRF/djangoDRF/urls.py
+++ django-tutorials/djangoDRF/djangoDRF/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", include("drfapp.urls")),
+    path("api/token/", TokenObtainPairView.as_view(), name = "token"),
+    path("api/toke/refresh/", TokenRefreshView.as_view(), name="refresh"),
 ]
--- django-tutorials/djangoDRF/drfapp/views.py
+++ django-tutorials/djangoDRF/drfapp/views.py
@@ -1,25 +1,29 @@
 from django.shortcuts import render
-
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 
 
-@api_view['POST']
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+@authentication_classes([JWTAuthentication])
 def create_note(request):
     pass
 
-@api_view['GET']
+@api_view(['GET'])
 def get_note(request):
     pass
 
 
-@api_view['PATCH']
+@api_view(['PATCH'])
 def update_note(request):
     pass
 
 
-@api_view['DELETE']
+@api_view(['DELETE'])
 def delete_note(request):
     pass
 
