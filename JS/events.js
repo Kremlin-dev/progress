18c09dca31af03225b57e5986c91d7af4dac111b
Kremlin-dev <yawamp27@gmail.com>
2025-03-01 21:54:54 +0000

    events in js

---
+++ JS/events.js
@@ -0,0 +1,14 @@
+// HTML events are "things" that happen to HTML elements.
+
+// Javascript lets you execute code when events are detected, something like clicking a button or when a page refreshesetc
+
+//General syntax for adding events = < element event = 'some js code'>
+
+/*
+onchange	An HTML element has been changed
+onclick	The user clicks an HTML element
+onmouseover	The user moves the mouse over an HTML element
+onmouseout	The user moves the mouse away from an HTML element
+onkeydown	The user pushes a keyboard key
+onload	The browser has finished loading the page
+*/
